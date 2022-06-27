1d76629e482b480219a4117c37bf1980a9d15020
RafayWaqar99 <raffaywaqar792@gmail.com>
2022-06-27 10:02:24 +0500

    update

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-# os.environ["PATH"] += os.pathsep + 'C:\Users\raffa\Downloads\windows_10_msbuild_Release_graphviz-4.0.0-win32\Graphviz'
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
